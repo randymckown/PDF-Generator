1c25611243319c9d063db89adff67bba1becbc7c
Randy McKown <randymckown@me.com>
2024-11-12 08:00:28 -0600

    Initial Commit

---
+++ PDF Generator/PDF_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDF_GeneratorApp.swift
+//  PDF Generator
+//
+//  Created by Randy McKown on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PDF_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
